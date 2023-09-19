e1df337b0d878a4617d0015ac71a1685812ddac4
Min <ka4697@gmail.com>
2023-09-19 10:51:45 +0900

    settings update

--- techit/settings/base.py
+++ techit/settings/base.py
@@ -18,20 +18,6 @@ from django.urls import reverse_lazy
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1nr6-589u1-%893*qp)uewlkat$h0m$1q+)#&+tf)_0-bis7&m'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    '*'
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -84,17 +70,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'techit.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -144,15 +119,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
 LOGOUT_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
-
-
-
-
-
-
-
-
-
-
-
-
