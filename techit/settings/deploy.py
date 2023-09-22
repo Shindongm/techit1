82f8832bb53bbb1759f1c8a18e328fa40d3bb3d6
Min <ka4697@gmail.com>
2023-09-22 11:23:30 +0900

    settings update

--- techit/settings/deploy.py
+++ techit/settings/deploy.py
@@ -1,5 +1,14 @@
 from .base import *
 
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.lstrip(), rstrip()
+    file.close()
+    return secret
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -18,8 +27,12 @@ ALLOWED_HOSTS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": read_secret("MARIADB_DATABASE"),
+        "USER": read_secret("MARIADB_USER"),
+        "PASSWORD": read_secret("MARIADB_PASSWORD"),
+        "HOST": "mariadb",
+        "PORT": "3306",
     }
 }
